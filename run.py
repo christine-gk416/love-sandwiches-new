bbf3fd3e3065d6c5a73c356362e4ce9874c50c7e
Christine Greaney-Kelley <kelleyc416@gmail.com>
2022-04-13 13:18:24 +0000

    Add ability to print current stock data in the terminal

--- run.py
+++ run.py
@@ -138,6 +138,20 @@ def calculate_stock_data(data):
     return new_stock_data
 
 
+def get_stock_values(data):
+    """
+    Function to get current stock values from the Google sheet
+    """
+    headings = SHEET.worksheet('stock').row_values(1)
+
+    stock_dict = {key: value for key, value in zip(headings, data)}
+
+    for key in stock_dict:
+        print('There are ' + str(stock_dict[key])  + ' of ' + key + ' left')
+
+    return stock_dict
+
+
 def main():
     """
     Run all program functions
@@ -150,6 +164,8 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, 'stock')
+    stock_values = get_stock_values(stock_data)
+
 
 print("Welcome to Love Sandwiches Data Automation")
 main()
